27c414e65b27bacbd7254e96e018120bf184347b
mcteach21 <mc.teach.21@gmail.com>
2022-05-28 11:00:50 +0200

    mc component add

---
+++ src/app/mc/mc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mc',
+  templateUrl: './mc.component.html',
+  styleUrls: ['./mc.component.css']
+})
+export class McComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
